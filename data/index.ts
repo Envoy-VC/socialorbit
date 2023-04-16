e79f157e34b471db39ade19b7a0a0b28c954e3fb
Vedant Chainani <vedantchainani1084@gmail.com>
2023-04-16 19:04:16 +0530

    Add Custom Authentication with iron-session and siwe

--- data/index.ts
+++ data/index.ts
@@ -15,3 +15,11 @@ export const navItems = [
 		url: '/more',
 	},
 ];
+
+export const ironOptions = {
+	cookieName: 'siwe',
+	password: 'pDx]FE>LuDXXLeEBruo2!o,e77%}T-B4et-mym7UTFER^p-Uwi:-ti0-~>3L)wq=',
+	cookieOptions: {
+		secure: process.env.NODE_ENV === 'production',
+	},
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import type { AppProps } from 'next/app';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 
 import {
+	createAuthenticationAdapter,
+	RainbowKitAuthenticationProvider,
+	AuthenticationStatus,
 	getDefaultWallets,
 	RainbowKitProvider,
-	Theme,
 	darkTheme,
 } from '@rainbow-me/rainbowkit';
 
@@ -13,6 +16,9 @@ import { polygon, polygonMumbai } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
+// swie Imports
+import { SiweMessage } from 'siwe';
+
 // Styles
 import '@/styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
@@ -20,10 +26,7 @@ import '@rainbow-me/rainbowkit/styles.css';
 // Configure Wagmi
 const { chains, provider } = configureChains(
 	[polygon, polygonMumbai],
-	[
-		alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY as string }),
-		publicProvider(),
-	]
+	[publicProvider()]
 );
 const { connectors } = getDefaultWallets({
 	appName: 'socialorbit',
@@ -38,20 +41,111 @@ const wagmiClient = createClient({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+	const fetchingStatusRef = useRef(false);
+	const verifyingRef = useRef(false);
+	const [authStatus, setAuthStatus] = useState<AuthenticationStatus>('loading');
+
+	// Fetch user when:
+	useEffect(() => {
+		const fetchStatus = async () => {
+			if (fetchingStatusRef.current || verifyingRef.current) {
+				return;
+			}
+
+			fetchingStatusRef.current = true;
+
+			try {
+				const response = await fetch('/api/me');
+				const json = await response.json();
+				setAuthStatus(json.address ? 'authenticated' : 'unauthenticated');
+			} catch (_error) {
+				setAuthStatus('unauthenticated');
+			} finally {
+				fetchingStatusRef.current = false;
+			}
+		};
+
+		// 1. page loads
+		fetchStatus();
+
+		// 2. window is focused (in case user logs out of another window)
+		window.addEventListener('focus', fetchStatus);
+		return () => window.removeEventListener('focus', fetchStatus);
+	}, []);
+
+	const authAdapter = useMemo(() => {
+		return createAuthenticationAdapter({
+			getNonce: async () => {
+				const response = await fetch('/api/nonce');
+				return await response.text();
+			},
+
+			createMessage: ({ nonce, address, chainId }) => {
+				return new SiweMessage({
+					domain: window.location.host,
+					address,
+					statement: 'Sign in with Ethereum to the app.',
+					uri: window.location.origin,
+					version: '1',
+					chainId,
+					nonce,
+				});
+			},
+
+			getMessageBody: ({ message }) => {
+				return message.prepareMessage();
+			},
+
+			verify: async ({ message, signature }) => {
+				verifyingRef.current = true;
+
+				try {
+					const response = await fetch('/api/verify', {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({ message, signature }),
+					});
+
+					const authenticated = Boolean(response.ok);
+
+					if (authenticated) {
+						setAuthStatus(authenticated ? 'authenticated' : 'unauthenticated');
+					}
+
+					return authenticated;
+				} catch (error) {
+					return false;
+				} finally {
+					verifyingRef.current = false;
+				}
+			},
+
+			signOut: async () => {
+				setAuthStatus('unauthenticated');
+				await fetch('/api/logout');
+			},
+		});
+	}, []);
+
 	return (
 		<WagmiConfig client={wagmiClient}>
-			<RainbowKitProvider
-				chains={chains}
-				theme={darkTheme({
-					accentColor: '#8B5CF6',
-					accentColorForeground: 'white',
-					borderRadius: 'medium',
-					fontStack: 'system',
-					overlayBlur: 'small',
-				})}
+			<RainbowKitAuthenticationProvider
+				adapter={authAdapter}
+				status={authStatus}
 			>
-				<Component {...pageProps} />
-			</RainbowKitProvider>
+				<RainbowKitProvider
+					chains={chains}
+					theme={darkTheme({
+						accentColor: '#8B5CF6',
+						accentColorForeground: 'white',
+						borderRadius: 'medium',
+						fontStack: 'system',
+						overlayBlur: 'none',
+					})}
+				>
+					<Component {...pageProps} />
+				</RainbowKitProvider>
+			</RainbowKitAuthenticationProvider>
 		</WagmiConfig>
 	);
 }
